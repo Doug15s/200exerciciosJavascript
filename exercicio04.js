cf2f2c0dfdfe62896879189e7ed8e1d605b27c88
Douglas de Barros <douglasbl18@gmail.com>
2025-09-17 09:28:17 -0300

    Declare duas variáveis, nome e sobrenome, atribua o seu nome e sobrenome. Concatene-as em uma variável chamada nomeCompleto e imprima.

---
+++ exercicio04.js
@@ -0,0 +1,7 @@
+// Exercício 4: Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima.
+
+let nome = "Douglas";
+let sobrenome = "de Barros Lemes";
+let nomeCompleto = nome + " " + sobrenome;
+
+console.log(`Meu nome completo é: ${nomeCompleto}.`);
