2f49b4c24e6622d167ff6b37f619db22b56a3083
Douglas de Barros <douglasbl18@gmail.com>
2025-09-18 17:44:12 -0300

    Declare três variáveis e atribua valores numéricos. Use operadores de comparação para comparar os valores. Imprima os resultados.

---
+++ exercicio07.js
@@ -0,0 +1,30 @@
+// Exercício 7: Declare três variáveis e atribua valores numéricos a elas. Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados.
+
+let numerico1 = 10;
+let numerico2 = 20;
+let numerico3 = 30;
+
+console.log(numerico1 == numerico2);
+console.log(numerico1 == numerico3);
+console.log(numerico3 == numerico2);
+console.log(numerico1 != numerico2);
+console.log(numerico1 != numerico3);
+console.log(numerico3 != numerico2);
+console.log(numerico1 === numerico2);
+console.log(numerico1 === numerico3);
+console.log(numerico3 === numerico2);
+console.log(numerico1 !== numerico2);
+console.log(numerico1 !== numerico3);
+console.log(numerico3 !== numerico2);
+console.log(numerico1 > numerico2);
+console.log(numerico1 > numerico3);
+console.log(numerico3 > numerico2);
+console.log(numerico1 >= numerico2);
+console.log(numerico1 >= numerico3);
+console.log(numerico3 >= numerico2);
+console.log(numerico1 <= numerico2);
+console.log(numerico1 <= numerico3);
+console.log(numerico3 <= numerico2);
+console.log(numerico1 < numerico2);
+console.log(numerico1 < numerico3);
+console.log(numerico3 < numerico2);
