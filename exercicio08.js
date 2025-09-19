cb015ded99c1dbbb0a512adf09576bac147921ae
Douglas de Barros <douglasbl18@gmail.com>
2025-09-19 16:50:34 -0300

    Declare uma variável e atribua um valor numérico a ela. Use o operador de incremento para aumentar o valor. Imprima o resultado.

---
+++ exercicio08.js
@@ -0,0 +1,7 @@
+// Exercício 8: Declare uma variável e atribua um valor numérico a ela. Use o operador de incremento para aumentar o valor da variável. Imprima o resultado.
+
+let numerico = 1;
+
+numerico++;
+
+console.log(numerico);
