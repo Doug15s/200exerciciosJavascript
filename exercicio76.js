cdb50f2093f2f25310a1aa5027f08e4c6f0cb045
Douglas de Barros <douglasbl18@gmail.com>
2025-10-15 18:57:15 -0300

    Crie um objeto vazio e adicione três propriedades a ele, cada uma com diferentes tipos de valores.

---
+++ exercicio76.js
@@ -0,0 +1,7 @@
+// Exercício 76: Crie um objeto vazio e adicione três propriedades a ele, cada uma com diferentes tipos de valores.
+
+let carro = {
+    modelo: "Corolla",
+    fabricante: "Toyota",
+    anoModelo: "2020/2021"
+};
